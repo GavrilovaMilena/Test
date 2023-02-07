5da29faa354aee74dc8f50fc1984532a8229951a
Gavrilova Milena <72129731+GavrilovaMilena@users.noreply.github.com>
2023-02-07 20:40:47 +0300

    Add slider

--- index.js
+++ index.js
@@ -7,3 +7,77 @@ $('.reserve').click(function() {
 $('.popup-close').click(function() {
     $('.popup-overlay, .popup-window').removeClass('show');
 })
+
+function myPlugin({ swiper, extendParams, on }) {
+    extendParams({
+        debugger: false,
+    });
+
+    on('init', () => {
+        if (!swiper.params.debugger) return;
+        console.log('init');
+    });
+    on('click', (swiper, e) => {
+        if (!swiper.params.debugger) return;
+        console.log('click');
+    });
+    on('tap', (swiper, e) => {
+        if (!swiper.params.debugger) return;
+        console.log('tap');
+    });
+    on('doubleTap', (swiper, e) => {
+        if (!swiper.params.debugger) return;
+        console.log('doubleTap');
+    });
+    on('sliderMove', (swiper, e) => {
+        if (!swiper.params.debugger) return;
+        console.log('sliderMove');
+    });
+    on('slideChange', () => {
+        if (!swiper.params.debugger) return;
+        console.log(
+            'slideChange',
+            swiper.previousIndex,
+            '->',
+            swiper.activeIndex
+        );
+    });
+    on('slideChangeTransitionStart', () => {
+        if (!swiper.params.debugger) return;
+        console.log('slideChangeTransitionStart');
+    });
+    on('slideChangeTransitionEnd', () => {
+        if (!swiper.params.debugger) return;
+        console.log('slideChangeTransitionEnd');
+    });
+    on('transitionStart', () => {
+        if (!swiper.params.debugger) return;
+        console.log('transitionStart');
+    });
+    on('transitionEnd', () => {
+        if (!swiper.params.debugger) return;
+        console.log('transitionEnd');
+    });
+    on('fromEdge', () => {
+        if (!swiper.params.debugger) return;
+        console.log('fromEdge');
+    });
+    on('reachBeginning', () => {
+        if (!swiper.params.debugger) return;
+        console.log('reachBeginning');
+    });
+    on('reachEnd', () => {
+        if (!swiper.params.debugger) return;
+        console.log('reachEnd');
+    });
+}
+new Swiper('.swiper', {
+    // Install Plugin To Swiper
+    modules: [myPlugin],
+    navigation: {
+        nextEl: '.right-arrow',
+        prevEl: '.left-arrow',
+    },
+    // Enable debugger
+    debugger: true,
+});
